7f44f7ac9ee84e9088624e5d57594220f7446983
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2023-06-03 15:14:42 +0700

    fix: ZK-4325 graceful client shutdown

--- zookeeper/ZK-4325/trigger_ZK-4325.sh
+++ zookeeper/ZK-4325/trigger_ZK-4325.sh
@@ -4,11 +4,11 @@ cd "$1" || exit
 
 sudo ./bin/zkServer.sh start || exit
 
-(echo "create /a"; echo "create /a/b"; echo "ls /"; echo "ls /a"; echo "ls /a/b"; echo "deleteall /") | sudo ./bin/zkCli.sh -server 127.0.0.1:2181
+(echo "create /a"; echo "create /a/b"; echo "ls /"; echo "ls /a"; echo "ls /a/b"; echo "deleteall /"; echo "quit") | sudo ./bin/zkCli.sh -server 127.0.0.1:2181
 
 echo -e "////////////////////////////////////////////////\n"
 
-echo "Check that when we did \"ls /a\", only /a is shown, /a/b is not shown"
+echo "Check that when we did \"ls /\", only /a is shown, /a/b is not shown"
 echo "Also check when we did \"deleteall /\", an error occured"
 
 echo -e "\n////////////////////////////////////////////////\n"
